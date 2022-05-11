cc15d0be345708dd6bcb1f14ef01e64767ce052e
Adenekan Usman <mr.nekan123@gmail.com>
2022-05-11 22:16:11 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCartItems, calculateTotals } from "./features/cart/cartSlice";
+
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [dispatch, cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading)
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
